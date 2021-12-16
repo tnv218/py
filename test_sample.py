d01cd30c90fcd09290a86e920e7604e3338e846a
tnv218 <tnv218@gmail.com>
2021-12-17 00:19:09 +0700

    1

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,10 @@
-def inc(x):
-    return x + 1
+import pytest
 
 
-def test_answer():
-    assert inc(3) == 4
+def f():
+    raise SystemExit(1)
+
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
